0290183366f64d54808f5c1f4fcca7fcde49d556
aditya-nitorinfotech <125881795+aditya-nitorinfotech@users.noreply.github.com>
2023-02-21 15:18:18 +0530

    Add files via upload

---
+++ custom-API-Service/src/api/posts-report/routes/posts-report.js
@@ -0,0 +1,14 @@
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/posts-report",
+      handler: "posts-report.postsReport",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+};
+
