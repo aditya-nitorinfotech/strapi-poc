b78a0d2f42423e0097cf8e63c2b5c3a848e4fb7c
aditya-nitorinfotech <125881795+aditya-nitorinfotech@users.noreply.github.com>
2023-03-14 17:05:36 +0530

    Update database.js

--- shopping-app/strapi-server/config/database.js
+++ shopping-app/strapi-server/config/database.js
@@ -1,11 +1,38 @@
-const path = require('path');
+// const path = require('path');
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+// strapi-api/config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'shoppingItem'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'admin123'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: env.bool('DATABASE_SSL', false) && {
+        key: env('DATABASE_SSL_KEY', undefined),
+        cert: env('DATABASE_SSL_CERT', undefined),
+        ca: env('DATABASE_SSL_CA', undefined),
+        capath: env('DATABASE_SSL_CAPATH', undefined),
+        cipher: env('DATABASE_SSL_CIPHER', undefined),
+        rejectUnauthorized: env.bool(
+          'DATABASE_SSL_REJECT_UNAUTHORIZED',
+          true, 'DATABASE_SSL_SELF', false
+        ),
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
